943b449f6a772111dfcb8558385e243c9d0ede8c
Peyton <83377575+PeytonIsHere@users.noreply.github.com>
2024-12-13 17:16:22 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function toggleMenu() {
     const menu = document.getElementById("dropdown-menu");
     menu.classList.toggle("hidden");
 }
+function copyWalletAddress() {
+    var walletAddress = document.getElementById("wallet-address").innerText;
+    var textArea = document.createElement("textarea");
+    textArea.value = walletAddress;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+}
